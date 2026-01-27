93cf0462736562e6e4dc38884b4f8b343f46c5cb
mcwintal <mcwintal@st.swps.edu.pl>
2026-01-27 20:37:43 +0100

    dodano aplikacje

---
+++ gr_1_projekt/gr_1_projekt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gr_1_projekt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gr_1_projekt.settings')
+
+application = get_asgi_application()
---
+++ gr_1_projekt/wypozyczalnia_filmow/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WypozyczalniaFilmowConfig(AppConfig):
+    name = 'wypozyczalnia_filmow'
